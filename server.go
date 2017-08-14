dcac2051f94042f7d040bfe21d4fda5c37292fd4
sjfricke <sjfricke@wisc.edu>
2017-08-13 22:56:58 -0700

    Go server working

---
+++ server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/index.html");
+
+	router.Static("/libs", "./libs");
+	router.Static("/models", "./models");
+	router.Static("/js", "./js");
+	router.Static("/styles", "./styles");
+	
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK, "index.html", gin.H{
+				"test" : "123",
+			},
+		)
+	})
+	
+	router.Run() // listen and serve on 0.0.0.0:8080
+}
